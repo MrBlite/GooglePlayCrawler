545a6f289a670762ce80045752b40a626e4b2a94
mrblite <terrylee9712@outlook.com>
2018-11-15 19:48:14 +0800

    '1'

--- GooglePlay/settings.py
+++ GooglePlay/settings.py
@@ -85,13 +85,14 @@ DOWNLOADER_MIDDLEWARES = {
 }
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 64
-
+# The maximum number of concurrent (ie. simultaneous) requests that will be performed by the Scrapy downloader.100
+CONCURRENT_ITEMS = 400
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-# CONCURRENT_REQUESTS_PER_DOMAIN = 64
+CONCURRENT_REQUESTS_PER_DOMAIN = 32
 # CONCURRENT_REQUESTS_PER_IP = 64
 
 # Disable cookies (enabled by default)
